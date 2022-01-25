e8a57d198cc8138d926149f9a3ce5dedaae49d25
aaronespasa <aaespasa@gmail.com>
2022-01-25 22:18:14 +0100

    Notes on how to create an animated background with glassmorphism

--- learningSwiftUI/learningSwiftUI/BadgeWithBlueBackground.swift
+++ learningSwiftUI/learningSwiftUI/BadgeWithBlueBackground.swift
@@ -10,13 +10,15 @@ import SwiftUI
 struct BadgeWithBlueBackground: View {
     var body: some View {
         ZStack {
-            Rectangle()
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .ignoresSafeArea()
+            AnimateBackground()
+                .ignoresSafeArea(.all)
+                .blur(radius: 50)
+                .scaleEffect(1.5)
             VStack {
-                Circle()
-                    .stroke(Color.black, lineWidth: 2)
-                    .frame(width: 44, height: 44)
+                Image("studio")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+                    .clipShape(Circle())
                 Text("Aaron Espasandin")
                     // list of fonts: https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/typography/
                     .font(.custom("Lobster-Regular", size: 32))
@@ -24,24 +26,46 @@ struct BadgeWithBlueBackground: View {
                     //.lineSpacing(10.0)
                     //.lineLimit(1)
                     //.multilineTextAlignment(.center)
-                Text("UI Designer")
+                Text("Computer Vision Engineer")
                     .foregroundColor(Color.secondary)
+                Divider()
                 Capsule()
-                    .fill(Color.green)
+                    .fill(Color.blue)
                     .frame(height: 44)
                     .overlay(
-                        Text("Sign up")
+                        Text("Follow")
                             .foregroundColor(Color.white)
                             .bold()
                     )
             }
             .padding()
-            .frame(width: 300, height: 200)
-            .background(Color.white)
+            .frame(width: 300, height: 250)
+            .background(.thinMaterial)
             .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
         }
         
     }
+}
+
+struct AnimateBackground: View {
+    @State var start = UnitPoint(x: 0, y: -2)
+    @State var end = UnitPoint(x: 4, y: 0)
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .default).autoconnect()
+    let colors = [Color.blue, Color.purple, Color.pink]
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: colors), startPoint: start, endPoint: end)
+            .animation(Animation.easeInOut(duration: 3).repeatForever())
+            .onReceive(timer, perform: { _ in
+                self.start = UnitPoint(x: 4, y: 0)
+                self.end = UnitPoint(x: 0, y: 2)
+                self.start = UnitPoint(x: -4, y: 20)
+                self.start = UnitPoint(x: 4, y: 0)
+            })
+        
+    }
+    
 }
 
 struct BadgeWithBlueBackground_Previews: PreviewProvider {
