247aeafd419653c3514037ccc2de704443497ca3
aaronespasa <aaespasa@gmail.com>
2022-01-25 18:55:21 +0100

    Initial files

---
+++ learningSwiftUI/learningSwiftUI/learningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningSwiftUIApp.swift
+//  learningSwiftUI
+//
+//  Created by Aarón Espasandín on 25/1/22.
+//
+
+import SwiftUI
+
+@main
+struct learningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
