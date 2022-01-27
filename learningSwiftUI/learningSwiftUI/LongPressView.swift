a79a4ad922488c1b1163c3d4c5651630bc2c963e
aaronespasa <aaespasa@gmail.com>
2022-01-27 10:10:19 +0100

    Notes on how to capture a long press gesture

---
+++ learningSwiftUI/learningSwiftUI/LongPressView.swift
@@ -0,0 +1,37 @@
+//
+//  LongPressView.swift
+//  learningSwiftUI
+//
+//  Created by Aarón Espasandín on 27/1/22.
+//
+
+import SwiftUI
+
+struct LongPressView: View {
+    @GestureState var press = false
+    @State var show = false
+    
+    var body: some View {
+        Image(systemName: "camera.fill")
+            .foregroundColor(.white)
+            .frame(width: 60, height: 60)
+            .background(show ? Color.black : Color.blue)
+            .mask(Circle())
+            .scaleEffect(press ? 2 : 1)
+            .animation(Animation.spring(), value: press)
+            .gesture(
+                LongPressGesture(minimumDuration: 0.5).updating($press) { currentState, gestureState, transaction in
+                    gestureState = currentState
+                }
+                .onEnded {value in
+                    show.toggle()
+                }
+            )
+    }
+}
+
+struct LongPressView_Previews: PreviewProvider {
+    static var previews: some View {
+        LongPressView()
+    }
+}
